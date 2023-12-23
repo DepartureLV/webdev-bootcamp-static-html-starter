daebb64ddd2cad7cb35a85933c69ab90d7179a31
Nattawat Pitikomon <nattawat.arch@gmail.com>
2023-12-23 13:46:22 +0700

    initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
       colors: {
-        primary: "#DEF81D",
-        "wd-white": "#FFFFFF",
-        "wd-grey": "#999999",
-        "wd-darkgrey": "#2D2D2D",
-        "wd-black": "#080808",
+        primary: '#DEF81D',
+        'wd-white': '#FFFFFF',
+        'wd-grey': '#999999',
+        'wd-darkgrey': '#2D2D2D',
+        'wd-black': '#080808',
       },
       backgroundImage: {
-        "hero-pattern":
-          "linear-gradient(to left, rgba(0,0,0, 0), rgba(0,0,0,1)), url('/assets/hero/hero.jpg')",
+        'hero-pattern':
+          "linear-gradient(to left, rgba(0,0,0, 0), rgba(0,0,0,1)), url('../assets/hero/hero.jpg')",
+      },
+      fontFamily: {
+        sans: ['poppins', 'arial', 'sans-serif'],
       },
     },
   },
